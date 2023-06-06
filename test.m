cfb4cc63033c18040db775a79d446103a08a9117
Vitaly Magerya <magv@tx97.net>
2023-06-06 19:58:04 +0200

    Test replacement rules

--- test.m
+++ test.m
@@ -43,6 +43,28 @@ FailUnless[
   {x1 + x2, x1 x2, {x1, x2}}]
 
 FailUnless[
+  Feynson["ufx -q -", {
+      {(l)^2-mm, (l-p1)^2-mm, (l-p1-p2)^2-mm},
+      {l},
+      {{p1^2, p1sq}, {p2^2, p2sq}, {p1*p2, p3sq/2 - p1sq/2 - p2sq/2}}
+  }] ===
+  {
+    x1 + x2 + x3,
+    -mm*x1^2 -mm*x2^2 -mm*x3^2
+    -2*mm*x1*x2 -2*mm*x1*x3 -2*mm*x2*x3
+    +p1sq*x1*x2 +p3sq*x1*x3 +p2sq*x2*x3,
+    {x1, x2, x3}
+  }]
+
+FailUnless[
+  Feynson["ufx -q -", {
+      {(l)^2, (l-p1)^2, (l-p1-p2)^2, (l-p1-p2-p3)^2},
+      {l},
+      {{p1^2, 0}, {p2^2, 0}, {p3^2, 0}, {p1*p2, s/2}, {p1*p3, t/2}, {p2*p3, u/2}}
+    }] ===
+  {x1 + x2 + x3 + x4, s x1 x3 + s x1 x4 + t x1 x4 + u x1 x4 + u x2 x4, {x1, x2, x3, x4}}]
+
+FailUnless[
   Feynson["zero-sectors -q -", { {(q-l)^2, l^2}, {0, 0}, {l}, {} }] ===
   {0, 1, 2}]
 
